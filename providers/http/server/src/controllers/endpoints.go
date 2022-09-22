5d7ed9dff90fa9c61c052ffe1b125f8583f01ac2
Yetkin Timocin <ytimocin@gmail.com>
2022-09-22 13:16:15 -0700

    Initial work on the endpoint Save (#22)

--- providers/http/server/src/controllers/endpoints.go
+++ providers/http/server/src/controllers/endpoints.go
@@ -15,3 +15,15 @@ func FindEndpoints(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"data": endpoints})
 }
+
+// POST /Save
+// Save an endpoint
+func SaveEndpoint(c *gin.Context) {
+	var saveReq models.SaveRequest
+	if err := c.ShouldBindJSON(&saveReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"data": saveReq})
+}
